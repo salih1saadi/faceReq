86f6e6d7362c247f4d076fb48f7cbeef801fbf43
salih saadi <salih-sadi@hotmail.com>
2020-01-28 21:20:55 +0200

    tyui

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -28,13 +28,13 @@ return (
    <p onClick= {()=>onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
 
 </nav>
-<h1 className='f1 f-headline lh-solid near-white '>{'Welcome To Recognition App'}</h1>
-<div className='ma4 mt0'>
+<h1 className='f1 f-headline lh-solid '>{'Welcome To Recognition App'}</h1>
+<div className='ma4 mt0 center'>
  <Tilt className="Tilt br3 shadow-3" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
   <div className="Tilt-inner pa4"> <img style={{paddingTop:'5px'}} src={brain} alt='logo'/> </div>
  </Tilt>
 </div>
-<p className='f3 ba shadow-5 b--black-10  center'>
+<p className='f3 ba shadow-5 b--black-10 mw7  center'>
       {'This Magic App will detect faces in picture and if the picture for a celeb person click recognize and it will tells you his/her name . Go and try!'}
    </p>
    </div>
