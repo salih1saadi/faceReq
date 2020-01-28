c5f9c553ca15220ea4652d5299b965bffebcef91
salih saadi <salih-sadi@hotmail.com>
2020-01-28 21:31:13 +0200

    dasdad

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () =>{
 
 return(
 
-<div className='ma4 mt0'>
+<div className='ma4 mt0 center'>
  <Tilt className="Tilt br3 shadow-3" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
   <div className="Tilt-inner pa4"> <img style={{paddingTop:'5px'}} src={brain} alt='logo'/> </div>
  </Tilt>
